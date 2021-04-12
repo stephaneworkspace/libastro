bfdc15f74976e569b68e0c1ca7cc84236b4c8134
stephaneworkspace <stephane@bressani.dev>
2021-04-12 21:58:11 +0200

    .so staticlib

--- build_without_appstore_compatibility.sh
+++ build_without_appstore_compatibility.sh
@@ -6,10 +6,10 @@ cargo test
 cargo build --target aarch64-apple-ios
 cargo build --target x86_64-apple-ios
 # Print NonFat -> Ok
-lipo -info ./target/aarch64-apple-ios/debug/liblibastro.a
-lipo -info ./target/x86_64-apple-ios/debug/liblibastro.a
+lipo -info ./target/aarch64-apple-ios/debug/liblibastro.so
+lipo -info ./target/x86_64-apple-ios/debug/liblibastro.so
 
 # Group in one
-lipo -create ./target/aarch64-apple-ios/debug/liblibastro.a ./target/x86_64-apple-ios/debug/liblibastro.a -output ./target/libastro.a
+lipo -create ./target/aarch64-apple-ios/debug/liblibastro.so ./target/x86_64-apple-ios/debug/liblibastro.so -output ./target/libastro.so
 # Print architecture
-lipo -info ./target/libastro.a
+lipo -info ./target/libastro.so
