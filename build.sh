ea02682b4026e7b68b0dd29631882b9c0dc82a84
stephaneworkspace <stephane@bressani.dev>
2021-04-13 13:00:35 +0200

    fix script build.sh

--- build.sh
+++ build.sh
@@ -2,16 +2,15 @@
 cargo build
 cargo test
 
-# TODO non debug, release
-
 # Group binary with lipo
 cargo +ios-arm64-nightly-2021-02-25 build --target aarch64-apple-ios --release --lib
-cargo build --target x86_64-apple-ios
+#cargo build --target x86_64-apple-ios
 # Print NonFat -> Ok
-lipo -info ./target/aarch64-apple-ios/debug/liblibastro.a
-lipo -info ./target/x86_64-apple-ios/debug/liblibastro.a
+lipo -info ./target/aarch64-apple-ios/release/liblibastro.a
+#lipo -info ./target/x86_64-apple-ios/release/liblibastro.a
 
 # Group in one
-lipo -create ./target/aarch64-apple-ios/debug/liblibastro.a ./target/x86_64-apple-ios/debug/liblibastro.a -output ./target/libastro.a
+#lipo -create ./target/aarch64-apple-ios/release/liblibastro.a ./target/x86_64-apple-ios/release/liblibastro.a -output ./target/libastro.a
+lipo -create ./target/aarch64-apple-ios/release/liblibastro.a -output ./target/libastro.a
 # Print architecture
 lipo -info ./target/libastro.a
